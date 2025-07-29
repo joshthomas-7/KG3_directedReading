f8e6adda8c011abb0b755428fe9ccfe29e8cc87d
joshthomas-7 <c3376353@uon.edu.au>
2025-07-29 12:56:42 +1000

    Computed the direct kinematics for the KG3, excluding the T78 transformation for now.

---
+++ Simulations/Kinematics/KG3_kinematicsCalc.m
@@ -0,0 +1,87 @@
+%%% Joshua Thomas
+%%% C3376353
+
+clear
+clc
+
+syms q [7 1]
+
+function y = c(x)
+    y = cos(x);
+end
+
+function y = s(x)
+    y = sin(x);
+end
+
+
+%% Creating the generic rotation matrix for each q
+n = length(q);
+Rs = {};
+for i = 1:n
+    R = [c(q(i)), -s(q(i)), 0, 0;
+         s(q(i)),  c(q(i)), 0, 0;
+         0,        0,       1, 0;
+         0,        0,       0, 1];
+    Rs{i} = R;
+end
+
+%% Creating the coordinate transformations
+Ts = {};
+
+T01 = [1 0 0 0;
+       0 -1 0 0;
+       0 0 -1 0.1564;
+       0 0 0 1];
+Ts{1} = T01;
+
+T12 = [1 0 0 0;
+       0 0 -1 0.0054;
+       0 1 0 -0.1284;
+       0 0 0 1];
+Ts{2} = T12;
+
+T23 = [1 0 0 0;
+    0 0 1 -0.2104;
+    0 -1 0 -0.00640;
+    0 0 0 1];
+Ts{3} = T23;
+
+T34 = [1 0 0 0;
+    0 0 -1 0.0064;
+    0 1 0 -0.2104;
+    0 0 0 1];
+Ts{4} = T34;
+
+T45 = [1 0 0 0;
+    0 0 1 -0.2084;
+    0 -1 0 -0.0064;
+    0 0 0 1];
+Ts{5} = T45;
+
+T56 = [1 0 0 0;
+    0 0 -1 0;
+    0 1 0 -0.1059;
+    0 0 0 1];
+Ts{6} = T56;
+
+T67 = [1 0 0 0;
+    0 0 1 -0.1059;
+    0 -1 0 0;
+    0 0 0 1];
+Ts{7} = T67;
+
+T78 = [1 0 0 0;
+    0 -1 0 0;
+    0 0 -1 -0.0615];
+
+%% Computing the transformation matrices
+T07 = eye(4);
+for j = 1:n
+    A = vpa(Ts{j} * Rs{j}, 4);
+    T07 = T07*A;
+    disp(['A' num2str(j-1) num2str(j) ' = ']);
+    disp(A);
+end
+
+T07 = simplify(T07, 'Steps', 15)
