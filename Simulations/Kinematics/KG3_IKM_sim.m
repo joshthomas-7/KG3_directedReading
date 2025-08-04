0ba893be38b3c0a319cf3dd569efc4fca9182afc
joshthomas-7 <c3376353@uon.edu.au>
2025-08-04 19:43:34 +1000

    Constructed an IKM algorithm using fmincon while only considering the end effector position. The orientation will be added soon.

--- Simulations/Kinematics/KG3_IKM_sim.m
+++ Simulations/Kinematics/KG3_IKM_sim.m
@@ -12,18 +12,59 @@ rho = 0.1;
 [transform_computed, Ts, Rs, As] = KG3_FKM;
 
 % Extracting the position vector
-f = transform_computed(1:end-1,end);
-
-%% Constructing the IKM optimisation problem
-% Uses sigma = [q s];
-W = eye(Ndof); % joint weighting matrix
-H = [W zeros(Ndof);
-     zeros(Ndof) zeros(Ndof)];
-f = [zeros(1,Ndof) rho*ones(1,Ndof)]
-
+r08 = transform_computed(1:end-1,end);
 
 %% Setting up the robot model
 % Load the KG3 robot 
 gen3 = loadrobot("kinovaGen3", "DataFormat", "column");
 % show(gen3);
 showdetails(gen3)
+
+%% Place the KG3 in a random configuration
+q_config = randomConfiguration(gen3);
+% show(gen3, q_config)
+
+%% Find the homogenous transform
+transform = getTransform(gen3, q_config, "EndEffector_Link");
+r08_desired = transform(1:end-1,end);
+
+%% Constructing the IKM optimisation problem
+% Converting the symbolic expression for the position vector to a function
+k = matlabFunction(r08);
+
+% Defining the objective function 
+function f = objfun(qtilde)
+    Ndof = 7;
+    W = eye(Ndof); % joint weighting matrix
+    f = qtilde.'*W*qtilde;
+end
+
+qtilde = optimvar('qtilde', 7);
+
+% Setting constraints
+qtilde.LowerBound = -pi;
+qtilde.UpperBound = pi;
+posConstraint = k(qtilde(1),qtilde(2),qtilde(3),qtilde(4),qtilde(5),qtilde(6)) == r08_desired;
+
+% Setting the inital conditions
+x0.qtilde = zeros(7,1);
+
+obj = objfun(qtilde);
+prob = optimproblem('Objective', obj);
+prob.Constraints.constr = posConstraint;
+
+% Solve the optimization problem
+[sol, fval] = solve(prob, x0);
+
+% Display the results
+disp('Optimal solution:');
+disp(sol);
+disp('Objective function value at optimal solution:');
+disp(fval);
+
+q = sol.qtilde;
+r08_achieved = k(q(1),q(2),q(3),q(4),q(5),q(6))
+r08_desired
+
+posError = abs(r08_achieved - r08_desired)
+
