fa8014924df16a2cbba84e08e0840cda39bb3576
joshthomas-7 <c3376353@uon.edu.au>
2025-08-06 09:55:35 +1000

    FKM sim now places the robot in multiple random configurations and plots the computed and true orientation for all of the configurations.

--- Simulations/Kinematics/KG3_kinematicSim.m
+++ Simulations/Kinematics/KG3_kinematicSim.m
@@ -20,40 +20,152 @@ showdetails(gen3)
 % Get the default joint configuration (home position)
 % q_config = gen3.homeConfiguration
 
+Niter = 10;
+pose_des_arr = zeros(7, Niter);
+pose_comp_arr = zeros(7, Niter);
 
+Ts = KG3_preCompute();
+
+for i=1:Niter
 %% Place the KG3 in a random configuration
 q_config = randomConfiguration(gen3);
-show(gen3, q_config)
-% 
+% show(gen3, q_config)
+
 %% Find the homogenous transform
-% transform = getTransform(gen3, q_config, "EndEffector_Link")
-transform = getTransform(gen3, q_config, "end_effector_link")
-p_des = transform(1:end-1,end);
-R_desired = transform(1:3,1:3);
-q_des = rotm2quat(R_desired);
+    % transform = getTransform(gen3, q_config, "EndEffector_Link")
+    transform = getTransform(gen3, q_config, "end_effector_link");
+    p_des = transform(1:end-1,end);
+    R_desired = transform(1:3,1:3);
+    q_des = rotm2quat(R_desired);
+    
+    
+    % q1 = q_config(1);
+    % q2 = q_config(2);
+    % q3 = q_config(3);
+    % q4 = q_config(4);
+    % q5 = q_config(5);
+    % q6 = q_config(6);
+    % q7 = q_config(7);
+    % 
+    % Using the joint positions to compute the expected transform
+    % transform_computed = vpa(subs(transform_computed),4)
+    
+    % % Extracting the pose from the homogenous transform matricies
+    % p_comp = double(transform_computed(1:end-1,end));
+    % R_comp = double(transform_computed(1:3,1:3));
+    % q_comp = rotm2quat(R_comp);
+
+    [p_comp, R_comp] = KG3_FKM_simple(q_config, Ts);
+    q_comp = rotm2quat(R_comp);
+    
+    % Computing the desired pose and the computed pose
+    pose_des = [p_des;q_des.'];
+    pose_comp = [p_comp;q_comp.'];
+
+    pose_des_arr(:,i) = pose_des;
+    pose_comp_arr(:,i) = pose_comp;
+    
+    % disp('True Pose & Computed Pose')
+    % disp([pose_des pose_comp])
+    % 
+    % % Finding the error between the poses
+    % pose_err = pose_des - pose_comp
+
+end
+
+%% Plotting
+configurations = linspace(1,Niter,Niter);
+
+figure(1)
+subplot(3,1,1)
+hold on
+title('End Effector Position Comparison', 'FontSize', 20)
+plot(configurations, pose_des_arr(1,:), 'LineWidth', 3)
+plot(configurations, pose_comp_arr(1,:), 'LineWidth', 3, 'LineStyle','--')
+ylabel('x Position', 'FontSize', 18)
+legend('True', 'Computed', 'FontSize', 16)
+
+subplot(3,1,2)
+hold on
+plot(configurations, pose_des_arr(2,:), 'LineWidth', 3)
+plot(configurations, pose_comp_arr(2,:), 'LineWidth', 3, 'LineStyle','--')
+ylabel('y Position', 'FontSize', 18)
+legend('True', 'Computed', 'FontSize', 16)
+
+subplot(3,1,3)
+hold on
+plot(configurations, pose_des_arr(3,:), 'LineWidth', 3)
+plot(configurations, pose_comp_arr(3,:), 'LineWidth', 3, 'LineStyle','--')
+ylabel('z Position', 'FontSize', 18)
+xlabel('Configuration Number', 'FontSize', 18)
+legend('True', 'Computed', 'FontSize', 16)
 
 
-q1 = q_config(1);
-q2 = q_config(2);
-q3 = q_config(3);
-q4 = q_config(4);
-q5 = q_config(5);
-q6 = q_config(6);
-q7 = q_config(7);
+figure(2)
+subplot(4,1,1)
+hold on
+title('End Effector Orientation Comparison', 'FontSize', 20)
+plot(configurations, pose_des_arr(4,:), 'LineWidth', 3)
+plot(configurations, pose_comp_arr(4,:), 'LineWidth', 3, 'LineStyle','--')
+ylabel('\eta', 'FontSize', 18)
+legend('True', 'Computed', 'FontSize', 16)
 
-% Using the joint positions to compute the expected transform
-transform_computed = vpa(subs(transform_computed),4)
-% Extracting the pose from the homogenous transform matricies
-p_comp = double(transform_computed(1:end-1,end));
-R_comp = double(transform_computed(1:3,1:3));
-q_comp = rotm2quat(R_comp);
+subplot(4,1,2)
+hold on
+plot(configurations, pose_des_arr(5,:), 'LineWidth', 3)
+plot(configurations, pose_comp_arr(5,:), 'LineWidth', 3, 'LineStyle','--')
+ylabel('\epsilon_1', 'FontSize', 18)
+legend('True', 'Computed', 'FontSize', 16)
 
-% Computing the desired pose and the computed pose
-pose_des = [p_des;q_des.'];
-pose_comp = [p_comp;q_comp.'];
+subplot(4,1,3)
+hold on
+plot(configurations, pose_des_arr(6,:), 'LineWidth', 3)
+plot(configurations, pose_comp_arr(6,:), 'LineWidth', 3, 'LineStyle','--')
+ylabel('\epsilon_2', 'FontSize', 18)
+xlabel('Configuration Number', 'FontSize', 18)
 
-disp('True Pose & Computed Pose')
-disp([pose_des pose_comp])
+subplot(4,1,4)
+hold on
+plot(configurations, pose_des_arr(7,:), 'LineWidth', 3)
+plot(configurations, pose_comp_arr(7,:), 'LineWidth', 3, 'LineStyle','--')
+ylabel('\epsilon_3', 'FontSize', 18)
+xlabel('Configuration Number', 'FontSize', 18)
+legend('True', 'Computed', 'FontSize', 16)
 
-% Finding the error between the poses
-pose_err = pose_des - pose_comp
+
+% %% Place the KG3 in a random configuration
+% q_config = randomConfiguration(gen3);
+% show(gen3, q_config)
+% % 
+% %% Find the homogenous transform
+% % transform = getTransform(gen3, q_config, "EndEffector_Link")
+% transform = getTransform(gen3, q_config, "end_effector_link")
+% p_des = transform(1:end-1,end);
+% R_desired = transform(1:3,1:3);
+% q_des = rotm2quat(R_desired);
+% 
+% 
+% q1 = q_config(1);
+% q2 = q_config(2);
+% q3 = q_config(3);
+% q4 = q_config(4);
+% q5 = q_config(5);
+% q6 = q_config(6);
+% q7 = q_config(7);
+% 
+% % Using the joint positions to compute the expected transform
+% transform_computed = vpa(subs(transform_computed),4)
+% % Extracting the pose from the homogenous transform matricies
+% p_comp = double(transform_computed(1:end-1,end));
+% R_comp = double(transform_computed(1:3,1:3));
+% q_comp = rotm2quat(R_comp);
+% 
+% % Computing the desired pose and the computed pose
+% pose_des = [p_des;q_des.'];
+% pose_comp = [p_comp;q_comp.'];
+% 
+% disp('True Pose & Computed Pose')
+% disp([pose_des pose_comp])
+% 
+% % Finding the error between the poses
+% pose_err = pose_des - pose_comp
